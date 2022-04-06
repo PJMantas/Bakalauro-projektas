bdd2ff0695431dc53b405c8e58c6f415bde9d549
PJMantas <manteliux6@gmail.com>
2022-04-06 22:17:05 +0300

    Ngx videogular simple implementation

--- sphereWatch/src/app/app-routing.module.ts
+++ sphereWatch/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { VideoViewComponent } from './components/video-view/video-view.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: SigninComponent },
   { path: 'register', component: SignupComponent },
   { path: 'profile', component: UserProfileComponent },
+  { path: 'videoView', component: VideoViewComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sphereWatch/src/app/app.module.ts
+++ sphereWatch/src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AuthInterceptor } from './shared/auth.interceptor';
+import { VideoViewComponent } from './components/video-view/video-view.component';
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     SignupComponent,
     UserProfileComponent,
+    VideoViewComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +27,10 @@ import { AuthInterceptor } from './shared/auth.interceptor';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
   providers: [
     {
